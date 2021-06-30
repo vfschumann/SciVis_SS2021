3302585fc1e9e3e85358528fb82e29365f615b63
Vic-Fabienne Schumann <vfschumann@icloud.com>
2021-06-30 12:05:01 +0200

    index: first ideas on renderConnectionLines

--- assignment7/assignment7_code_skeleton/src/renderers.js
+++ assignment7/assignment7_code_skeleton/src/renderers.js
@@ -18,21 +18,27 @@ export function renderAtoms( atom_list, scene ) {
         let position = atom_list[i].pos_vector
         const element = atom_list[i].elem;
         const color = atom_colors[element];
-        const radius = vanDerWaal_radii[element];
+        const radius = (vanDerWaal_radii[element] * 0.25);
 
         const sphereGeometry = new THREE.IcosahedronGeometry( radius, 3 );
         const material = new THREE.MeshPhongMaterial( { color: color } );
         const object = new THREE.Mesh( sphereGeometry, material );
         object.position.copy( position );
-     //   object.position.multiplyScalar( 75 );
-     //   object.scale.multiplyScalar( 25 );
         scene.add( object )
     }
 }
 
 // CONNECTIONS
 // TODO: function -> render connections as lines
+export function renderConnectionLines( connectionList, scene ) {
 
+    // following: https://threejs.org/docs/#manual/en/introduction/Drawing-lines
+    const material = new THREE.LineBasicMaterial( {color: 0x000000} );
+    const geometry = new THREE.BufferGeometry().setFromPoints( connectionList ); // maybe since this is a directory that doesn't work, better to give start and end point?
+    const line = new THREE.Line( geometry, material );
+
+    scene.add( line )
+}
 
 // TODO: function -> render connections as cylinders
 
